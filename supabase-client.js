6c9025bebc02ca4a2f1c9e32932f58acb91b9f73
Nisk <itsnisk@gmail.com>
2025-09-04 19:17:44 +0100

    Create supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,7 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const SUPABASE_URL = 'https://whxmfpdmnsungcwlffdx.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoeG1mcGRtbnN1bmdjd2xmZmR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMDk3MzYsImV4cCI6MjA3MTg4NTczNn0.PED6DKwmfzUFLIvNbRGY2OQV5XXmc8WKS9E9Be6o8D8';
+
+// Create and export the single supabase client instance
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
