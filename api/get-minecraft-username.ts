713da460114d138e5e06c94b64b6028edbf2bdff
Nisk <itsnisk@gmail.com>
2025-10-01 16:50:13 +0100

    Delete api/get-minecraft-username.ts

--- api/get-minecraft-username.ts
+++
@@ -1,8 +0,0 @@
-import cookie from 'cookie';
-
-export default async function handler(req, res) {
-  const cookies = cookie.parse(req.headers.cookie || '');
-  const ign = cookies.minecraft_ign || null;
-
-  res.json({ name: ign });
-}
